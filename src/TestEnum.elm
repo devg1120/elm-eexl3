6248193646e4a2c5d84cd3f621c7c397584083f5
devg1120 <devg1120@gmail.com>
2021-01-07 23:03:06 +0900

    enum test

--- src/TestEnum.elm
+++ src/TestEnum.elm
@@ -5,7 +5,7 @@ module TestEnum exposing (..)
 import Array
 import Dict exposing (Dict)
 
-
+{--
 enum_reg name entry dic =
     let
          new_dic = Dict.insert name entry dic
@@ -45,17 +45,18 @@ enum_count  dic =
 -----------------------------------------------
 enumdic_empty = Dict.empty
 
-enum_name = "Sushi"
-enum_entry = Array.fromList ["Tako","Ika","Tai","Tamago","Maguro"]
+enum_name1 = "Sushi"
+enum_entry1 = Array.fromList ["Tako","Ika","Tai","Tamago","Maguro"]
 
 enum_name2 = "Car"
 enum_entry2 = Array.fromList ["Honda","Toyota","Suzuki","Nissan","Matsuda"]
 
 enum_name3 = "City"
-enum_entry3 = Array.fromList ["Tokyo","Osaka","Kobe","Fukuoka","Nara"]
+--enum_entry3 = Array.fromList ["Tokyo","Osaka","Kobe","Fukuoka","Nara"]
+enum_entry3 = Array.fromList ["Tokyo","Osaka","Kobe","Fukuoka"]
 
 r0 = enum_count enumdic_empty
-enumdic = enum_reg enum_name enum_entry enumdic_empty
+enumdic = enum_reg enum_name1 enum_entry1 enumdic_empty
              |> enum_reg enum_name2 enum_entry2
              |> enum_reg enum_name3 enum_entry3
 
@@ -63,7 +64,82 @@ r1 = enum_count enumdic
 r2 = enum_get "City" "Kobe" enumdic
 r3 = enum_get "Sushi" "Kobe" enumdic
 r4 = enum_get "No" "Kobe" enumdic
+--}
+-----------------------------------------------------
+enum_reg name entry dic =
+    let
+        (new_dic,num) = 
+           case (Dict.get "_" dic) of
+                     Just a ->
+                              let
+                                n_ = Dict.size a
+                                n = n_ + 1
+                              in
+                              (Dict.insert "_" (Dict.insert name (0,n) a ) dic ,n)
+                     Nothing ->
+                              let
+                               d = Dict.empty
+                              in
+                              (Dict.insert "_" (Dict.insert name (0,1) d)  dic ,1)
+
+        entry_dic_empty = Dict.empty
+        f name_ (dic_, n) =
+              let
+                 n2 = n + 1
+              in
+              (Dict.insert name_ (num,n2) dic_, n2)
+              
+        (entry_dic, len)  = Array.foldl f (entry_dic_empty, 0) entry
+
+    in
+    Dict.insert name entry_dic new_dic
 
 
+enum_get2 name entry dic  =
+    case (Dict.get name dic) of
+         Just a ->
+                  Dict.get  entry a  
+         Nothing ->
+                  Just (0,0)
 
+enum_get name_entry dic  =
+    let
+      args = Array.fromList (String.split ":" name_entry)
+      name  = Array.get 0 args
+      entry  = Array.get 1 args
+    in
+    case (name,entry) of
+      (Just name_,Just entry_) ->
+          case (Dict.get name_ dic) of
+               Just a ->
+                        Dict.get  entry_ a  
+               Nothing ->
+                        Just (0,0)
+      _ ->
+          Just (0,0)
 
+--enum_count2  dic =
+--    case (Dict.get "_" dic) of
+--         Just a ->
+--                  Array.length  a  
+--         Nothing ->
+--                  0
+--
+-----------------------------------------------
+
+enum_name1 = "Sushi"
+enum_entry1 = Array.fromList ["Tako","Ika","Tai","Tamago","Maguro"]
+
+enum_name2 = "Car"
+enum_entry2 = Array.fromList ["Honda","Toyota","Suzuki","Nissan","Matsuda"]
+
+enum_name3 = "City"
+--enum_entry3 = Array.fromList ["Tokyo","Osaka","Kobe","Fukuoka","Nara"]
+enum_entry3 = Array.fromList ["Tokyo","Osaka","Kobe","Fukuoka"]
+
+enumdic = enum_reg enum_name1 enum_entry1 Dict.empty
+       |> enum_reg enum_name2 enum_entry2
+       |> enum_reg enum_name3 enum_entry3
+
+s2 = enum_get2 "City" "Kobe" enumdic
+s3 = enum_get "City:Kobe" enumdic
